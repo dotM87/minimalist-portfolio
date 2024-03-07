04974193ab5cb8fd894afa45fdd188c63d1726d9
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2024-03-07 00:26:57 -0400

    first-build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dotm87.github.io',
+    base: 'https://dotm87.github.io/portfolio'
+});
